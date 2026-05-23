838f4b058e4e2bf2e9754a9956e91779cbd40531
Alexandre Antunes <deco.xandyantunes@gmail.com>
2026-05-23 16:44:34 -0300

    agora vai

--- testeRegistro/admin-server.js
+++ testeRegistro/admin-server.js
@@ -7,7 +7,7 @@ const admin = require("firebase-admin");
 const fs = require("fs");
 
 // --- INICIALIZAÇÃO DO FIREBASE ---
-const serviceAccount = require("./firebase-service-account.json");
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
